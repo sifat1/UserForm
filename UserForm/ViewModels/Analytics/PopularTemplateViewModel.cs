41c46f815bd709c9dcc609d881fd7654658aa0f0
sifat1 <sifatshikdhar@gmail.com>
2025-06-29 23:05:58 +0600

    ui resued

--- UserForm/ViewModels/Analytics/PopularTemplateViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UserForm.ViewModels.Analytics;
-
-public class PopularTemplateViewModel
-{
-    public int FormId { get; set; }
-    public string FormTitle { get; set; }
-    public string OwnerEmail { get; set; }
-    public int SubmissionCount { get; set; }
-}
