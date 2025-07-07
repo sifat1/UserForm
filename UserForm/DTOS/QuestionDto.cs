abdaed25668fbcfefda1a680f6bae612feb2942f
sifat1 <sifatshikdhar@gmail.com>
2025-07-07 20:40:33 +0600

    fixed issues

--- UserForm/DTOS/QuestionDto.cs
+++ UserForm/DTOS/QuestionDto.cs
@@ -6,6 +6,6 @@ namespace UserForm.DTOS;
 public class QuestionDto
 {
     public string QuestionText { get; set; }
-    public string QuestionType { get; set; } // Text, Number, MultipleChoice
+    public string QuestionType { get; set; } 
     public List<string>? Options { get; set; }
 }
--- UserForm/Models/DBModels/Forms/FormTopicEntity.cs
+++
@@ -1,7 +0,0 @@
-namespace UserForm.Models.DBModels.Forms;
-
-public class FormTopicEntity
-{
-    public int Id {get; set;}
-    public string TopicName {get; set;}
-}
